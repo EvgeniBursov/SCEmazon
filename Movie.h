9d2b4f06369d17daba898a6645134e15d9b083ca
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2022-09-20 13:41:09 +0300

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+
+class Movie
+{
+protected:
+	string _nameMovie;
+	int _year;
+
+public:
+	Movie() {};
+	Movie(string name, int year) { _nameMovie = name; _year = year; }
+	Movie(const Movie& other) { _nameMovie = other._nameMovie; _year = other._year; }
+	virtual ~Movie() {}
+
+};
